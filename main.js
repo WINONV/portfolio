61779d72fd371a0059bfa61db48f35bb3240a20a
JIWON <hallns5227@gmail.com>
2022-02-09 07:35:37 +0900

    Javascript : navbar menu click

--- main.js
+++ main.js
@@ -3,11 +3,22 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    // console.log(window.scrollY);
-    console.log(navbarHeight);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    console.log(event.target.dataset.link);
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link==null){
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth'});
 });
